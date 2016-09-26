66f9fe45565d7a7abaa02874bd13cdb61762aeb4
Noah Berman <noah@noahberman.org>
2016-09-26 11:30:14 -0600

    working number guesser game

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"].to_i
+  message = guess_checker(guess)
+  number_reveal = " "
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
+
+def guess_checker(guess)
+  if (guess - SECRET_NUMBER) >= 6
+    message = "Way too high!"
+  elsif (SECRET_NUMBER - guess) >= 6
+    message = "Way too low!"
+  elsif (guess - SECRET_NUMBER) <= 5 && (guess - SECRET_NUMBER) > 0
+    message = "Too high!"
+  elsif (SECRET_NUMBER - guess) <= 5 && (SECRET_NUMBER - guess) > 0
+    message = "Too low!"
+  elsif guess == SECRET_NUMBER
+    message = "You got it right! The SECRET NUMBER was #{SECRET_NUMBER}!"
+  else
+    message = "Incorrect entry, try again."
+  end
+end
+
