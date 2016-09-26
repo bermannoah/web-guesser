fde60b87d6cea9874a0e1219645983bd5e1665d9
Noah Berman <noah@noahberman.org>
2016-09-26 11:43:59 -0600

    adds color-based extension, other rudimentary style changes

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,23 +6,30 @@ SECRET_NUMBER = rand(100)
 get '/' do
   guess = params["guess"].to_i
   message = guess_checker(guess)
-  number_reveal = " "
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  color = color
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message,
+                          :color => color}
 end
 
 def guess_checker(guess)
   if (guess - SECRET_NUMBER) >= 6
-    message = "Way too high!"
+    @color = "#ff0000"
+    @message = "Way too high!"
   elsif (SECRET_NUMBER - guess) >= 6
-    message = "Way too low!"
+    @color = "#ff0000"
+    @message = "Way too low!"
   elsif (guess - SECRET_NUMBER) <= 5 && (guess - SECRET_NUMBER) > 0
-    message = "Too high!"
+    @color = "#cb3535"
+    @message = "Too high!"
   elsif (SECRET_NUMBER - guess) <= 5 && (SECRET_NUMBER - guess) > 0
-    message = "Too low!"
+    @color = "#cb3535"
+    @message = "Too low!"
   elsif guess == SECRET_NUMBER
-    message = "You got it right! The SECRET NUMBER was #{SECRET_NUMBER}!"
+    @message = "You got it right! The SECRET NUMBER was #{SECRET_NUMBER}!"
+    @color = "#2bab25"
   else
-    message = "Incorrect entry, try again."
+    @message = "Incorrect entry, try again."
+    @color = "#ff0000"
   end
 end
 
